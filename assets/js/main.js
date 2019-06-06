9a007d041c1c44f0cf2ace367ba391320e06fa62
imeda <imedabeqaia@gmail.com>
2019-06-06 16:24:59 +0400

    6th commit
    add: menu responsive, header menu show

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,29 @@ $(document).ready(function(){
     $(".header-hamburg").click(function(){
        $(this).children().toggleClass("x-hamburg");
        setTimeout(function(){
+           
+        $(".header-menu-list").toggleClass("active-menu-resp");
         $(".header-hamburg").children().toggleClass("rotate-hamburg");
        }, 1000)
     })
+
 })
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    $(".header-menu").children().removeClass("active-menu-resp");
+    $(".header-hamburg").children().toggleClass("rotate-hamburg");
+    $(".header-hamburg").children().toggleClass("x-hamburg");
+    console.log("scroll down");
+  } else {
+      
+    console.log("scroll up");
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
